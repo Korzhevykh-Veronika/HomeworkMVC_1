b8fd65891262a1c79a943b614a0293f5fa4ce58e
VeronikaKorzhevyh <veronikakorzhevyh@gmail.com>
2020-02-02 13:55:05 +0200

    MVC

---
+++ Homework_mvc/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Guest()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Profile(string firstName)
+        {
+            return View("~/Views/Home/Result.cshtml");
+        }
+    }
+}
